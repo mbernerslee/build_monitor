6c796aba85ae44c931b12fe9845b45b3eed058fe
berners <matthewbernerslee@gmail.com>
2022-08-15 12:51:45 +0100

    x

--- mix.exs
+++ mix.exs
@@ -69,7 +69,7 @@ defmodule BuildMonitor.MixProject do
       setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      # test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.deploy": ["esbuild default --minify", "phx.digest"]
     ]
   end
